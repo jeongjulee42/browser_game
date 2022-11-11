590415440fc730265e8a65f65daecce1ccef5d51
jeongjulee <ljj3342@naver.com>
2022-11-11 14:19:33 +0900

    Feat: add timer

--- bro_game_advance/main.js
+++ bro_game_advance/main.js
@@ -2,6 +2,7 @@
 const CARROT_SIZE = 80;
 const CARROT_COUNT = 5;
 const BUG_COUNT = 5;
+const GAME_DURATION_SEC = 5;
 
 const field = document.querySelector('.game__field');
 const fieldRect = field.getBoundingClientRect();
@@ -32,6 +33,24 @@ function startGame(){
 function stopGame(){
 
 };
+
+function startGameTimer(){
+    let remainingTimeSec = GAME_DURATION_SEC;
+    updateTimerText(remainingTimeSec);
+    timer = setInterval(() => {
+        if(remainingTimeSec <= 0){
+            clearInterval(timer);
+            return;
+        }
+        updateTimerText(--remainingTimeSec);
+    }, 1000);
+}
+
+function updateTimerText(time){
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    gameTimer.innerText = `${minutes}:${seconds}`;
+}
 
 function showStopButton(){
     const icon = gameBtn.querySelector('.fa-play');
